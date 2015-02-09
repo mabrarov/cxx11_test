62532def1ae81841aee2173267f711833a75fbdc
Marat Abrarov <abrarov@gmail.com>
2015-02-09 14:14:23 +0300

    CMake build scripts fixed.

--- build/cmake/static_c_runtime_overrides.cmake
+++ build/cmake/static_c_runtime_overrides.cmake
@@ -1,3 +1,10 @@
+# 
+# Copyright (c) 2010-2015 Marat Abrarov (abrarov@gmail.com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
 if(MSVC)
     set(CMAKE_C_FLAGS_DEBUG_INIT          "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
     set(CMAKE_C_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
