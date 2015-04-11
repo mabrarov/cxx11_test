1c97ea6a896bb6653b6788b03936bb79072ec01e
Marat Abrarov <abrarov@gmail.com>
2015-04-11 21:47:59 +0300

    CMAke project: work with compiler / linker flags was made the same way as `asio-samples` project uses.

--- build/cmake/linker_options.cmake
+++ build/cmake/linker_options.cmake
@@ -1,11 +1,11 @@
-#
-# Copyright (c) 2015 Marat Abrarov (abrarov@gmail.com)
+# 
+# Copyright (c) 2010-2015 Marat Abrarov (abrarov@gmail.com)
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
 
-cmake_minimum_required(VERSION 2.8)
+cmake_minimum_required(VERSION 2.8.11)
 
 # Turn on Unicode support for MinGW for main function
 if(MINGW)
