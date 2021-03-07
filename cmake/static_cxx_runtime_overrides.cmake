293c2f31f3546e1d4b8272bc494c87c1702eee4a
Marat Abrarov <abrarov@gmail.com>
2021-03-07 16:43:37 +0300

    Fixed linking with static C/C++ runtime when using GCC / MinGW.

--- cmake/static_cxx_runtime_overrides.cmake
+++ cmake/static_cxx_runtime_overrides.cmake
@@ -17,7 +17,9 @@ if(MSVC)
 endif()
 
 if(CMAKE_COMPILER_IS_GNUCC)
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
 endif()
 
 set(Boost_USE_STATIC_LIBS    ON)
